79554f2cb3d083056f94dee9b8f49396e4e201b0
MohamedOrfy1 <mohamedorfy@ieee.org>
2025-05-19 13:19:54 +0300

    Defined App Routes

--- client/Masary/src/App.jsx
+++ client/Masary/src/App.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
-
+import { BrowserRouter as Router , Routes, Route, Navigate} from 'react-router-dom'
+import Login from './pages/Auth/Login'
+import SignUp from './pages/Auth/SignUp'
+import Income from './pages/Dashboard/Income'
+import Expense from './pages/Dashboard/Expense'
+import Home from './pages/Dashboard/Home'
 const App = () => {
   return (
-    <div className=''>
-      App
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Root />} />
+          <Route path="/login" exact element={<Login />} />
+          <Route path="/signup" exact element={<SignUp />} />
+          <Route path="/dashboard" exact element={<Home />} />
+          <Route path="/income" element={<Income />} />
+          <Route path="/expense" element={<Expense/>} />
+        </Routes>
+      </Router>
     </div>
   )
 }
 
 export default App
+
+const Root = () => {
+  // Check if the user is authenticated
+  const isAuthenticated = !!localStorage.getItem('token');
+  // Redirect to the login page if not authenticated
+  return isAuthenticated ? <Navigate to="/dashboard" /> : <Navigate to="/login" />;
+}
