e1beb55329d1a7fc97aff60af885b72897e19fb3
MohamedOrfy1 <mohamedorfy@ieee.org>
2025-05-19 23:24:23 +0300

    Finished Login page

--- client/Masary/src/pages/Auth/Login.jsx
+++ client/Masary/src/pages/Auth/Login.jsx
@@ -1,14 +1,28 @@
 import React, { useState } from 'react'
 import AuthLayout from '../../components/layouts/AuthLayout'
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Input from '../../components/inputs/Input';
+import { validateEmail } from '../../utils/helper';
 
 const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [error, setError] = useState(null);
     const navigate = useNavigate();
-    const handleLogin = async (e) => {}
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        if(!validateEmail(email)){
+            setError('Please enter a valid email address');
+            return;
+        }
+        if(!password){
+            setError('Please enter your password');
+            return;
+        }
+        setError("");
+        
+        //Login API call
+    }
   return (
     <AuthLayout>
       <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
@@ -42,7 +56,12 @@ const Login = () => {
                 className="btn-primary">
                 Login
             </button>
-            
+            <p className="text-[13px] text-slate-800 mt-3">
+                Don't have an account? 
+                <Link  className="font-medium text-primary underline" to="/signup">
+                    Sign up
+                </Link>
+            </p>
         </form>
       </div>
     </AuthLayout>
