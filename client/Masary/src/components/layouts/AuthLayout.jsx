304ddf1a4a837393948bf47a59f72a78e6629765
MohamedOrfy1 <mohamedorfy@ieee.org>
2025-05-19 20:47:43 +0300

    Input Component

--- client/Masary/src/components/layouts/AuthLayout.jsx
+++ client/Masary/src/components/layouts/AuthLayout.jsx
@@ -27,7 +27,7 @@ const AuthLayout = ({children}) => {
         className="w-64 lg:w-[90%] absolute bottom-10 shadow-lg shadow-blue-400/15"
       />
     </div>
-  </div>
+  </div> 
   );
 };
 
--- client/Masary/src/pages/Auth/Login.jsx
+++ client/Masary/src/pages/Auth/Login.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AuthLayout from '../../components/layouts/AuthLayout'
+import { useNavigate } from 'react-router-dom';
+import Input from '../../components/inputs/Input';
 
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState(null);
+    const navigate = useNavigate();
+    const handleLogin = async (e) => {}
   return (
     <AuthLayout>
       <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
@@ -11,6 +18,32 @@ const Login = () => {
         <p className="text-xs text-slate-700 mt-[5px] mb-6">
           Please login to your account  
         </p>
+        
+        <form onSubmit={handleLogin}>
+            <Input
+                value={email}
+                onChange={({ target }) => setEmail(target.value)}
+                label="Email"
+                placeholder="john@example.com"
+                type="email"
+            />
+
+            <Input
+                value={password}
+                onChange={({ target }) => setPassword(target.value)}
+                label="Password"
+                placeholder="Min. 8 characters"
+                type="password" 
+            />
+
+            {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
+            <button
+                type="submit"
+                className="btn-primary">
+                Login
+            </button>
+            
+        </form>
       </div>
     </AuthLayout>
   )
