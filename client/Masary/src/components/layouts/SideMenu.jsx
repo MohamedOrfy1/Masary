df578ebdf0756ee26cc9f7de63c16ae07219bb06
MohamedOrfy1 <mohamedorfy@ieee.org>
2025-05-21 20:25:11 +0300

    Finished DashboardLayout Component

---
+++ client/Masary/src/components/layouts/SideMenu.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SideMenu = () => {
+    console.log("SideMenu rendered");
+  return (
+    <div>
+      <h1>Side Menu</h1>
+    </div>
+  )
+}
+
+export default SideMenu
